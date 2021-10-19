8048f37ae6a614f54f9c21c0a430b2c05837700f
Tallenbrook <tom.allenbrook@gmail.com>
2021-10-19 13:31:08 +0100

    initial website setup

---
+++ php/about.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<?php
+include_once('../html/navbar.html');
+?>
+
+<head>
+    <title>
+        webpage
+    </title>
+</head>
+<body style="background-color:dimgrey;text-align:center">
+welcome to my page
+</body>
+</html>
---
+++ php/main.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/homepage.css">
+<script src="../javascript/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js"></script>
+
+<?php
+include_once('../html/navbar.html');
+include_once('../javascript/p5.html');
+
+?>
+
+
+
+
+
+
+
+
+
+
